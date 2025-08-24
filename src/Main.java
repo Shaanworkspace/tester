41130594e106038209ef0ecb06940fe74b38f835
shaan yadav <shaanyworkspace@gmail.com>
2025-08-24 22:09:40 +0530

    ww

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,9 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("shaa");
         }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
